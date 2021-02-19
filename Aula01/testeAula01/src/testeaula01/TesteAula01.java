419af1e5710cd112fd9ddd8e74cc2120a2b12895
Fabio Lubacheski <50373829+flubacheski@users.noreply.github.com>
2021-02-19 09:11:41 -0300

    Add files via upload

---
+++ Aula01/testeAula01/src/testeaula01/TesteAula01.java
@@ -0,0 +1,12 @@
+package testeaula01;
+
+/**
+ *
+ * @author fabio
+ */
+public class TesteAula01 {
+    public static void main(String[] args) {
+        System.out.println("ola netbeans!");
+    }
+    
+}
